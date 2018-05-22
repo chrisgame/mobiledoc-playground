fac9845eb318d9ce79546004d77e66de4fb900cf
The Game <chris.game@robbiebobbins.com>
2018-05-22 11:33:59 +0100

    only show selection buttons when text is selected

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -4,6 +4,7 @@ import Renderer from 'mobiledoc-text-renderer';
 
 export default Controller.extend({
   mobiledocExample: null,
+  textSelected: false,
 
   mobiledocExampleInText: computed('mobiledocExample', function() {
     let mobiledocExample = this.get('mobiledocExample');
@@ -13,6 +14,13 @@ export default Controller.extend({
   }),
 
   actions: {
+    mobiledocEditorWasCreated(editor) {
+      this.set('editor', editor);
+
+      editor.cursorDidChange(() => {
+        this.set('textSelected', this.get('editor.range.direction'));
+      });
+    },
     mobiledocWasUpdated(updatedMobiledoc) {
       this.set('mobiledocExample', updatedMobiledoc);
     }
