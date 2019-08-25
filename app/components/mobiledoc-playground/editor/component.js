23b066762f09676934d81ec067efc6b6c70eb59d
The Game <chris.game@robbiebobbins.com>
2019-08-26 00:42:38 +0100

    register the card with the editor

--- app/components/mobiledoc-playground/editor/component.js
+++ app/components/mobiledoc-playground/editor/component.js
@@ -1,8 +1,16 @@
 import Component from '@ember/component';
+import createComponentCard from 'ember-mobiledoc-editor/utils/create-component-card';
+import { computed } from '@ember/object';
 
 export default Component.extend({
   mobiledoc: undefined,
   onMobiledocUpdated: () => {},
 
   tagName: '',
+
+  cards: computed(function() {
+    return [
+      createComponentCard('mobiledoc-playground/cards/code-block/card'),
+    ];
+  }),
 });
