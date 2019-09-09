141913543b7b00667dfdaabcac74f1cfd8b3e012
The Game <chris.game@robbiebobbins.com>
2019-09-09 09:46:05 +0100

    add the atom on text match

--- app/components/mobiledoc-playground/editor/component.js
+++ app/components/mobiledoc-playground/editor/component.js
@@ -5,4 +5,30 @@ export default Component.extend({
   onMobiledocUpdated: () => {},
 
   tagName: '',
+
+  actions: {
+    mobiledocEditorWasCreated(editor) {
+      this.set('editor', editor);
+
+      editor.onTextInput({
+        text: ' ember ',
+        run(postEditor) {
+          let lengthOfText = this.text.length;
+          let backwardsByTextLength = -Math.abs(lengthOfText);
+
+          postEditor.deleteRange(editor.range.extend(backwardsByTextLength));
+
+          editor.insertText(' ');
+          editor.insertAtom(
+            'mobiledoc-playground/atoms/logo/atom',
+            'ember',
+            {
+              imageUrl: 'https://emberjs.com/images/brand/ember_Ember-Dark-80b69143e1da02507f58f8de460a4072.png'
+            }
+          );
+          editor.insertText(' ');
+        }
+      });
+    },
+  }
 });
