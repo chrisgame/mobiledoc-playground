373cdfaf7fa859730723867e31be743134ef06a4
The Game <chris.game@robbiebobbins.com>
2019-09-10 09:09:25 +0100

    remove contextual toolbar to make base example clearer

--- app/components/mobiledoc-playground/editor/component.js
+++ app/components/mobiledoc-playground/editor/component.js
@@ -3,20 +3,6 @@ import Component from '@ember/component';
 export default Component.extend({
   mobiledoc: undefined,
   onMobiledocUpdated: () => {},
-  textSelected: false,
 
   tagName: '',
-
-  actions: {
-    mobiledocEditorWasCreated(editor) {
-      this.set('editor', editor);
-
-      editor.cursorDidChange(() => {
-        this.set('textSelected', this.get('editor.range.direction'));
-      });
-    },
-    mobiledocWasUpdated(updatedMobiledoc) {
-      this.set('mobiledoc', updatedMobiledoc);
-    }
-  }
 });
